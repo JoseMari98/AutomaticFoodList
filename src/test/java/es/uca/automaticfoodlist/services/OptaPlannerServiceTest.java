dd71db05d3d55c749119a54fa996b15be93df08b
Jose Mari <josemari-98@hotmail.com>
2020-05-26 18:42:33 +0200

    Added: -OptaPlanner configuration to test

--- src/test/java/es/uca/automaticfoodlist/services/OptaPlannerServiceTest.java
+++ src/test/java/es/uca/automaticfoodlist/services/OptaPlannerServiceTest.java
@@ -1,9 +1,14 @@
-package es.uca.automaticfoodlist.services;
+/*package es.uca.automaticfoodlist.services;
 
+import es.uca.automaticfoodlist.entities.HorarioComidas;
+import es.uca.automaticfoodlist.entities.UsuarioReceta;
 import org.junit.Test;
 import org.junit.jupiter.api.Timeout;
+import org.optaplanner.core.api.solver.SolverStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
 
 @SpringBootTest(properties = {
         "optaplanner.solver.termination.spent-limit=1h", // Effectively disable this termination in favor of the best-score-limit
@@ -17,7 +22,7 @@ public class OptaPlannerServiceTest {
     @Test
     @Timeout(600_000)
     public void solveDemoDataUntilFeasible() throws InterruptedException {
-        /*optaPlannerService.solve(usuarioService.loadUserByUsername("user"));
+        optaPlannerService.solve(usuarioService.loadUserByUsername("user"));
         HorarioComidas horarioComidas = optaPlannerService.getTimeTable(usuarioService.loadUserByUsername("user"));
         while (horarioComidas.getSolverStatus() != SolverStatus.NOT_SOLVING) {
             // Quick polling (not a Test Thread Sleep anti-pattern)
@@ -32,6 +37,7 @@ public class OptaPlannerServiceTest {
             assertNotNull(usuarioReceta.getUsuario());
             assertNotNull(usuarioReceta.getComida());
         }
-        assertTrue(horarioComidas.getScore().isFeasible());*/
+        assertTrue(horarioComidas.getScore().isFeasible());
     }
 }
+*/
